8a80eaad6c9816fb046df72d3e55faec2fc22632
Chris Larabee <chris.larabee9@gmail.com>
2020-03-30 19:27:50 -0500

    Renamed lib to service.

--- service/soap.py
+++ service/soap.py
@@ -7,3 +7,9 @@ client = Salesforce(
     password=SFCONFIG.password,
     security_token=SFCONFIG.security_token
 )
+
+accounts = client.bulk.Account.query('select Id, Name from Account limit 10')
+
+if __name__ == '__main__':
+    for a in accounts:
+        print(a)
