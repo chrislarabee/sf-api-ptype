8e28f60c249de78f72795c91f9c22872498566a4
Chris Larabee <chris.larabee9@gmail.com>
2020-03-31 15:20:24 -0500

    Moved select statement generation to the Table object and added docstrings to Codex and Table. Sundry improvements to the query handling to maximize convenience. Also clarified and standardized what the 'master key' for Salesforce objects is. Since this is accessing the api, the Codex and Tables will assume api_names are being used by the end user. The Salesforce instance UI labels are accessible if needed, but are secondarily used rather than inconsistently used.

--- service/test/codex_test.py
+++ service/test/codex_test.py
@@ -1,19 +1,14 @@
-from service.codex import Codex
+from service.codex import Table
 
 
-class TestCodex:
+class TestTable:
     def test_gen_limit(self):
-        assert Codex._gen_limit() == ''
-        assert Codex._gen_limit(10) == ' LIMIT 10'
-
-    def test_gen_query(self):
-        assert Codex._gen_query(
-            ['Id', 'Name'], 'Account', '', '') == (
-            'SELECT Id, Name FROM Account')
+        assert Table._gen_limit() == ''
+        assert Table._gen_limit(10) == ' LIMIT 10'
 
     def test_gen_where(self):
-        assert Codex._gen_where() == ''
-        assert Codex._gen_where('Id = 1') == ' WHERE Id = 1'
-        assert Codex._gen_where(
+        assert Table._gen_where() == ''
+        assert Table._gen_where('Id = 1') == ' WHERE Id = 1'
+        assert Table._gen_where(
             ['Id >= 1', 'Name = Test']
         ) == ' WHERE Id >= 1 AND Name = Test'
