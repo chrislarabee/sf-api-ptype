19dfba285df179f18f784256fd8f75b285fd1cd7
Chris Larabee <chris.larabee9@gmail.com>
2020-03-30 21:13:10 -0500

    In progress commit for improvements to the Codex object to make it more friendly for database discovery/exploration.

--- service/codex.py
+++ service/codex.py
@@ -13,19 +13,27 @@ class Codex:
         self._tables = dict()
         for o in self.client.describe()['sobjects']:
             if o['createable']:
-                self._tables[o['name']] = getattr(self.client, o['name'])
+                t = Table(getattr(self.client, o['name']))
+                self._tables[o['name']] = t
+                setattr(self, o['name'], t)
 
-    def schema(self, tablename: str):
-        return self._gen_schema(self._tables[tablename])
+
+class Table:
+    def __init__(self, sftype_obj: SFType):
+        self._sftype_obj = sftype_obj
+        self.cols, self.schema = self._gen_schema(sftype_obj)
 
     @staticmethod
     def _gen_schema(sftype_obj: SFType):
         cols = []
+        schema = dict()
         for f in sftype_obj.describe()['fields']:
-            d = dict(
-                label=f['label'],
-                name=f['name'],
-                type=f['type']
+            label = f['label']
+            name = f['name']
+            type_ = f['type']
+            schema[name] = dict(
+                label=label,
+                type=type_
             )
-            cols.append(d)
-        return cols
+            cols.append(label)
+        return cols, schema
